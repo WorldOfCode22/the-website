47b47facc9b90df414351ad00ccdc44d21890025
Sloan Gwaltney <gwaltneys2@winthrop.edu>
2018-12-08 16:30:12 -0500

    project card works

--- client/src/app/components/util/project-card/project-card.component.ts
+++ client/src/app/components/util/project-card/project-card.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ICard } from '../../routes/projects/projects.component';
+import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogPopupComponent } from '../dialog-popup/dialog-popup.component';
+
+export interface IDialogData {
+  link: string;
+}
 
 @Component({
   selector: 'app-project-card',
@@ -8,15 +15,38 @@ import { ICard } from '../../routes/projects/projects.component';
 })
 export class ProjectCardComponent implements OnInit {
 
-  constructor() { }
+  private popupActive = false;
+  constructor(private router: Router, private dialog: MatDialog) { }
 
   @Input() private card: ICard;
 
   get Title() { return this.card.title; }
   get Subtitle() { return this.card.subtitle; }
-  get ImageLink() { console.log(this.card); return this.card.image; }
+  get ImageLink() { return this.card.image; }
   get Content() { return this.card.content; }
   get AppLink() { return this.card.link; }
+  get LocalLink() { return this.card.localLink; }
+
+  private openDialog() {
+    const dialogRef = this.dialog.open(DialogPopupComponent, {
+      width: '500px',
+      data: {link: this.AppLink}
+    });
+
+    dialogRef.afterClosed().subscribe((val) => {
+      if (val === 'OK') {
+        window.open(this.AppLink, '_blank');
+      }
+    });
+  }
+
+  VisitOnClick() {
+    if (this.LocalLink) {
+      this.router.navigate([this.AppLink]);
+    } else {
+      this.openDialog();
+    }
+  }
 
   ngOnInit() {
   }

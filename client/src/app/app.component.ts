97a2046c42ab4344fa4927f479040a4f0ce8ab17
Sloan Gwaltney <gwaltneys2@winthrop.edu>
2018-12-10 01:20:10 -0500

    added auto scroll on route change

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'client';
+
+  OnActivate() {
+    const scrollToTop = window.setInterval(() => {
+      const yPostilion = window.pageYOffset;
+      if (yPostilion > 0) {
+          window.scrollTo(0, yPostilion - 20);
+      } else {
+          window.clearInterval(scrollToTop);
+      }
+    }, 20);
+  }
 }

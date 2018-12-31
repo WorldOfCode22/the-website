ba859016b5ec4eee422e5a8996b5ef14c69e8788
WorldOfCode22 <gwaltneys2@winthrop.edu>
2018-12-31 10:45:45 -0500

    ca now build for staging and production envs

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseApiUrl: 'https://nameless-basin-55880.herokuapp.com/public'
+  baseApiUrl: 'https://promotion.sloangwaltney.com/public'
 };
---
+++ client/src/environments/environment.stage.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    baseApiUrl: 'https://nameless-basin-55880.herokuapp.com/public'
+};

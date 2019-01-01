999247068213c07b6b27754ff1f4a8285bc3914d
WorldOfCode22 <gwaltneys2@winthrop.edu>
2019-01-01 01:12:56 -0500

    edited environments for new api

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseApiUrl: 'https://promotion.sloangwaltney.com/public'
+  baseApiUrl: 'https://promotion.sloangwaltney.com/'
 };
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  baseApiUrl: 'http://localhost:8080/public'
+  baseApiUrl: 'http://localhost:8080'
 };
 
 /*
